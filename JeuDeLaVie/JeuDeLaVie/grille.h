178127e2fc0f49189e064173f18f0f2723f63d5f
Théo Masson--Champy <theo.massonchampy@viacesi.fr>
2024-12-02 14:16:16 +0100

    Fix grille
    Récupération de la taille avec une structure

--- JeuDeLaVie/JeuDeLaVie/grille.h
+++ JeuDeLaVie/JeuDeLaVie/grille.h
@@ -3,53 +3,41 @@
 #include <vector>
 #include "etat_case.h"
 
-
-class Grille {
-public:
-    int largeur, hauteur, taille_grille;
-    bool error_marker = false;
-
-    std::vector<EtatCase> grille_def() {
-        while (!error_marker) {
-            std::cout << "Donnez la hauteur de la grille : ";
-            std::cin >> hauteur;
-            if (hauteur != 0) {
-                break;
-            }
-            else {
-                std::cout << "La valeur doit etre un nombre positif !";
-            }
-        }
-        while (!error_marker) {
-            std::cout << "Donnez la largeur de la grille : ";
-            std::cin >> largeur;
-            if (largeur != 0) {
-                break;
-            }
-            else {
-                std::cout << "La valeur doit �tre un nombre positif !";
-            }
-        }
-        std::vector<EtatCase> grille;
-        grille.resize(largeur * hauteur, EtatCase::MORT);
-
-
-        return ;
-    }
-    
-
-    std::vector<EtatCase> grille = grille_def();
-
-    //int lignes, colonnes;
-    //std::cout << "Combien de lignes souhaites-tu : ";
-    //std::cin >> lignes;
-
-    //std::cout << "Combien de colonnes souhaites-tu : ";
-    //std::cin >> colonnes;
-
-    //short grille[lignes * colonnes];
-    //for (int i = 0; i < lignes * colonnes; i++)
-        //grille[i] = 0;
-
-    Grille() = default;
+struct Grille {
+    int largeur;
+    int hauteur;
+    std::vector<EtatCase> grille;
 };
+
+Grille grille_def() {
+    int l = 0, h = 0;
+
+    while (true) {
+        std::cout << "Donnez la hauteur de la grille : ";
+        std::cin >> h;
+        if (h > 0) {
+            break;
+        }
+        else {
+            std::cout << "La valeur doit etre un nombre positif !" << std::endl;
+        }
+    }
+
+    while (true) {
+        std::cout << "Donnez la largeur de la grille : ";
+        std::cin >> l;
+        if (l > 0) {
+            break;
+        }
+        else {
+            std::cout << "La valeur doit etre un nombre positif !" << std::endl;
+        }
+    }
+
+    Grille resultat;
+    resultat.largeur = l;
+    resultat.hauteur = h;
+    resultat.grille.resize(l * h, EtatCase::MORT);
+
+    return resultat;
+}
