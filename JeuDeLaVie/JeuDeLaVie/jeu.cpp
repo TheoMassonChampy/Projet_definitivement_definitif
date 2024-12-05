87dc3b45a702e1b680fddc30ffcaca2a869cda5d
Théo Masson <theo.massonchampy@viacesi.fr>
2024-12-05 18:43:18 +0100

    Fonction verif + manager (base)

--- JeuDeLaVie/JeuDeLaVie/jeu.cpp
+++ JeuDeLaVie/JeuDeLaVie/jeu.cpp
@@ -3,52 +3,63 @@
 
 int Jeu::nombre_voisin(int location) {
 	int nb_voisin = 0;
-	if (location == 0) {
-		if (grilledejeu.grille[location + 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
+	if (location == 0) { //en haut à gauche
+		if ((grilledejeu.grille[location + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
 	}
-	else if (location == grilledejeu.largeur - 1) {
-		if (grilledejeu.grille[location - 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
+	else if (location == grilledejeu.largeur - 1) { //en haut à droite
+		if ((grilledejeu.grille[location - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
 	}
-	else if (location == (grilledejeu.largeur * grilledejeu.hauteur) - grilledejeu.largeur) {
-		if (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location + 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
+	else if (location == (grilledejeu.largeur * grilledejeu.hauteur) - grilledejeu.largeur) { //en bas à gauche
+		if ((grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
 	}
-	else if (location == (grilledejeu.largeur * grilledejeu.hauteur) - 1) {
-		if (grilledejeu.grille[location - 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
-		if (grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::VIVANT) {
-			nb_voisin += 1;
-		}
+	else if (location == (grilledejeu.largeur * grilledejeu.hauteur) - 1) { // en bas à droite
+		if ((grilledejeu.grille[location - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
 	}
-	else if () {
-
+	else if (location % grilledejeu.largeur == 0 && location != 0 && location == (grilledejeu.largeur * grilledejeu.hauteur) - grilledejeu.largeur) { //Première colonne (sans 0 et case en bas à gauche)
+		if ((grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+	}
+	else if ((location % grilledejeu.largeur) - 1 == 0) { //Dernière colonne (à droite)
+		if ((grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+	}
+	else if (location < grilledejeu.largeur) { //Première ligne
+		if ((grilledejeu.grille[location - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+	}
+	else if (location > (grilledejeu.largeur * grilledejeu.hauteur) - grilledejeu.largeur) {
+		if (grilledejeu.grille[location - 1] == EtatCase::VIVANT) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+	}
+	else {
+		if ((grilledejeu.grille[location - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location - grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur - 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + grilledejeu.largeur + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
+		if ((grilledejeu.grille[location + 1] == EtatCase::VIVANT) || (grilledejeu.grille[location + 1] == EtatCase::BLOC_VIVANT)) { nb_voisin += 1; }
 	}
 	return nb_voisin;
 }
@@ -58,4 +69,15 @@ void Jeu::manager_console() {
 	grilledejeu.Chargement_grille_fichier();
 	std::cout << "Chargement réussi ! Grille définie." << std::endl;
 	grilledejeu.affiche_grille(grilledejeu.grille_vers_nombre());
+	for (int i = 0; i < grilledejeu.largeur; i++) {
+		int voisins = nombre_voisin(i);
+		if ((grilledejeu.grille[i] != EtatCase::BLOC_MORT) || (grilledejeu.grille[i] != EtatCase::BLOC_VIVANT)) {
+			if (voisins == 3) {
+				if (grilledejeu.grille[i] == EtatCase::MORT) { grilledejeu.grille[i] == EtatCase::VIVANT; }
+			}
+			else if ((voisins < 2) || (voisins > 3)) {
+				if (grilledejeu.grille[i] == EtatCase::VIVANT) { grilledejeu.grille[i] == EtatCase::MORT; }
+			}
+		}
+	}
 }
